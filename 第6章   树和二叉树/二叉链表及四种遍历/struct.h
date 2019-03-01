9e2d9bcc2d52f75b83f6b697642ce6e3b0306b4e
zimuge <48090588+rzimuge@users.noreply.github.com>
2019-03-01 21:01:11 +0800

    Create struct.h

---
+++ 第6章   树和二叉树/二叉链表及四种遍历/struct.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+typedef char TElemType;/*The Tree Type*/
+typedef struct BiTNode
+{
+	TElemType data;
+	BiTNode *lchild, *rchild;
+}BiTNode, *BiTree;
+
+typedef BiTree StackType;/*The Stack Type*/
+
+
+typedef struct stack
+{
+	StackType sdata;
+	stack *back;
+}*Stack;
+typedef struct StackTop {
+	Stack top;
+}StackTop;
